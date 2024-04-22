b29841345a40462d92ea293e5b3ec732de7ebb24
EvgeniyHotetovskiy <squaer.r@mail.ru>
2024-04-22 21:55:33 +0300

    HomeWork

--- src/test/java/HomeWork1/SharelaneTests.java
+++ src/test/java/HomeWork1/SharelaneTests.java
@@ -11,17 +11,39 @@ import org.openqa.selenium.By;
 
 public class SharelaneTests {
     @Test
-    public void signUpPositiveTest() {
+    public void signUpPositiveTest1() {
         RemoteWebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.navigate().to("https://www.sharelane.com/cgi-bin/register.py");
         driver.findElement(By.name("zip_code")).sendKeys("12345");
         driver.findElement(By.cssSelector("input[value=Continue]")).click();
-        boolean isFirstNameDisplayed = driver.findElement(By.name("first_name")).isDisplayed();
+        driver.findElement(By.name("first_name")).sendKeys("Evgeniy");
+        driver.findElement(By.name("last_name")).sendKeys("Test");
+        driver.findElement(By.name("email")).sendKeys("1@mail.ru");
+        driver.findElement(By.name("password1")).sendKeys("1111");
+        driver.findElement(By.name("password2")).sendKeys("1111");
+        driver.findElement(By.cssSelector("input[value=Register]")).click();
+        boolean isFirstNameDisplayed = driver.findElement(By.cssSelector("span[class=confirmation_message]")).isDisplayed();
         Assert.assertTrue(isFirstNameDisplayed);
         driver.quit();
-
+    }
+    @Test
+    public void signUpPositiveTest2() {
+        RemoteWebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.navigate().to("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.name("zip_code")).sendKeys("12345");
+        driver.findElement(By.cssSelector("input[value=Continue]")).click();
+        driver.findElement(By.name("first_name")).sendKeys("Evgeniy");
+        driver.findElement(By.name("email")).sendKeys("1@mail.ru");
+        driver.findElement(By.name("password1")).sendKeys("1111");
+        driver.findElement(By.name("password2")).sendKeys("1111");
+        driver.findElement(By.cssSelector("input[value=Register]")).click();
+        boolean isFirstNameDisplayed = driver.findElement(By.cssSelector("span[class=confirmation_message]")).isDisplayed();
+        Assert.assertTrue(isFirstNameDisplayed);
+        driver.quit();
     }
 
     @Test
@@ -70,9 +92,14 @@ public class SharelaneTests {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.navigate().to("https://www.sharelane.com/cgi-bin/register.py");
-        driver.findElement(By.name("zip_code")).sendKeys("sd");
+        driver.findElement(By.name("zip_code")).sendKeys("12345");
         driver.findElement(By.cssSelector("input[value=Continue]")).click();
-        boolean isFirstNameDisplayed = driver.findElement(By.cssSelector("input[value=Continue]")).isDisplayed();
+        driver.findElement(By.name("first_name")).sendKeys("");
+        driver.findElement(By.name("email")).sendKeys("1@mail.ru");
+        driver.findElement(By.name("password1")).sendKeys("1111");
+        driver.findElement(By.name("password2")).sendKeys("1111");
+        driver.findElement(By.cssSelector("input[value=Register]")).click();
+        boolean isFirstNameDisplayed = driver.findElement(By.cssSelector("span[class=error_message]")).isDisplayed();
         Assert.assertTrue(isFirstNameDisplayed);
         driver.quit();
     }
@@ -83,9 +110,14 @@ public class SharelaneTests {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.navigate().to("https://www.sharelane.com/cgi-bin/register.py");
-        driver.findElement(By.name("zip_code")).sendKeys("ыв");
+        driver.findElement(By.name("zip_code")).sendKeys("12345");
         driver.findElement(By.cssSelector("input[value=Continue]")).click();
-        boolean isFirstNameDisplayed = driver.findElement(By.cssSelector("input[value=Continue]")).isDisplayed();
+        driver.findElement(By.name("first_name")).sendKeys("Evgeniy");
+        driver.findElement(By.name("email")).sendKeys("1mailru");
+        driver.findElement(By.name("password1")).sendKeys("1111");
+        driver.findElement(By.name("password2")).sendKeys("1111");
+        driver.findElement(By.cssSelector("input[value=Register]")).click();
+        boolean isFirstNameDisplayed = driver.findElement(By.cssSelector("span[class=error_message]")).isDisplayed();
         Assert.assertTrue(isFirstNameDisplayed);
         driver.quit();
     }
@@ -96,9 +128,65 @@ public class SharelaneTests {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.navigate().to("https://www.sharelane.com/cgi-bin/register.py");
-        driver.findElement(By.name("zip_code")).sendKeys("SELECT * FROM book");
+        driver.findElement(By.name("zip_code")).sendKeys("12345");
         driver.findElement(By.cssSelector("input[value=Continue]")).click();
-        boolean isFirstNameDisplayed = driver.findElement(By.cssSelector("input[value=Continue]")).isDisplayed();
+        driver.findElement(By.name("first_name")).sendKeys("Evgeniy");
+        driver.findElement(By.name("email")).sendKeys("1@mail.ru");
+        driver.findElement(By.name("password1")).sendKeys("1111");
+        driver.findElement(By.name("password2")).sendKeys("2222");
+        driver.findElement(By.cssSelector("input[value=Register]")).click();
+        boolean isFirstNameDisplayed = driver.findElement(By.cssSelector("span[class=error_message]")).isDisplayed();
+        Assert.assertTrue(isFirstNameDisplayed);
+        driver.quit();
+    }
+    @Test
+    public void signUpPNegativeTest7() {
+        RemoteWebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.navigate().to("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.name("zip_code")).sendKeys("12345");
+        driver.findElement(By.cssSelector("input[value=Continue]")).click();
+        driver.findElement(By.name("first_name")).sendKeys("Evgeniy");
+        driver.findElement(By.name("email")).sendKeys("1@mail.ru");
+        driver.findElement(By.name("password1")).sendKeys("1111");
+        driver.findElement(By.name("password2")).sendKeys("1111");
+        driver.findElement(By.cssSelector("input[value=Register]")).click();
+        boolean isFirstNameDisplayed = driver.findElement(By.cssSelector("span[class=error_message]")).isDisplayed();
+        Assert.assertTrue(isFirstNameDisplayed);
+        driver.quit();
+    }
+    @Test
+    public void signUpPNegativeTest8() {
+        RemoteWebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.navigate().to("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.name("zip_code")).sendKeys("12345");
+        driver.findElement(By.cssSelector("input[value=Continue]")).click();
+        driver.findElement(By.name("first_name")).sendKeys("Evgeniy");
+        driver.findElement(By.name("email")).sendKeys("1@mail.ru");
+        driver.findElement(By.name("password1")).sendKeys("");
+        driver.findElement(By.name("password2")).sendKeys("");
+        driver.findElement(By.cssSelector("input[value=Register]")).click();
+        boolean isFirstNameDisplayed = driver.findElement(By.cssSelector("span[class=error_message]")).isDisplayed();
+        Assert.assertTrue(isFirstNameDisplayed);
+        driver.quit();
+    }
+    @Test
+    public void signUpPNegativeTest9() {
+        RemoteWebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.navigate().to("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.name("zip_code")).sendKeys("12345");
+        driver.findElement(By.cssSelector("input[value=Continue]")).click();
+        driver.findElement(By.name("first_name")).sendKeys("Evgeniy");
+        driver.findElement(By.name("email")).sendKeys("1№;№%@mail.ru");
+        driver.findElement(By.name("password1")).sendKeys("1111");
+        driver.findElement(By.name("password2")).sendKeys("1111");
+        driver.findElement(By.cssSelector("input[value=Register]")).click();
+        boolean isFirstNameDisplayed = driver.findElement(By.cssSelector("span[class=error_message]")).isDisplayed();
         Assert.assertTrue(isFirstNameDisplayed);
         driver.quit();
     }
